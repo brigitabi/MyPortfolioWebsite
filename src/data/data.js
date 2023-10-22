55c698a418e997b645c8edd9954fef6ed8ccafcf
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-10-22 20:42:02 +0200

    prettier & new img

--- src/data/data.js
+++ src/data/data.js
@@ -7,81 +7,81 @@ import Crypto from '../assets/projects/crypto.jpeg';
 import GB2 from '../assets/projects/GB2.png';
 import Ramen from '../assets/projects/ramen.jpg';
 import Costs from '../assets/projects/costs.png';
-import Trello from '../assets/projects/trello.png';
+import Planning from '../assets/projects/planning.png';
 import Hireadmire from '../assets/projects/hireadmire.png';
 import Wisdomwonder from '../assets/projects/wisdomwonder.png';
 
-export const data=[
-      {
-        id:1,
-        name:"Wisdom Wonder",
-        image:Wisdomwonder,
-        github:"https://github.com/brigitabi/wisdom-wonder",
-        live:"https://wisdom-wonder.vercel.app",
-    },
-    {
-        id:2,
-        name:"Trello Clone with Chat GPT",
-        image:Trello,
-        github:"https://github.com/brigitabi/NetflixClone",
-        live:"https://trello-clone-piax.vercel.app",
-    },
-    {
-        id:3,
-        name:"Hire & Admire",
-        image:Hireadmire,
-        github:"https://github.com/brigitabi/H-A",
-        live:"https://h-a.vercel.app",
-    },
-    {
-        id:4,
-        name:"Netflix Clone",
-        image:Netflix,
-        github:"https://github.com/brigitabi/NetflixClone",
-        live:"https://netflix-clone-bribi.web.app/",
-    },
-    {
-        id:5,
-        name:"CoinNest",
-        image: Crypto,
-        github:"Lawork",
-        live:"https://cryptoexchange-2e20a.web.app/",
-    },
-    {
-        id:6,
-        name:"Global Beats",
-        image:GB2,
-        github:"https://github.com/brigitabi/globalBeats",
-        live:"https://global-beats.vercel.app/",
-    },
-    {
-        id:7,
-        name:"Expense Tracker",
-        image: Costs,
-        github:"https://github.com/brigitabi/expenseTrack",
-        live:"https://expense-track-ecru.vercel.app/",
-    },
-    {
-        id:8,
-        name:"We Eat",
-        image: Ramen,
-        github:"https://github.com/brigitabi/weEat",
-        live:"https://we-eat-gamma.vercel.app/",
-    },
-    {
-        id:9,
-        name:"Beautiful Image Slider",
-        image:Nature,
-        github:"https://github.com/brigitabi/slider",
-        live:"https://slider-flame.vercel.app",
-    },
-    {
-        id:10,
-        name:"Notes",
-        image: Goals,
-        github:"https://github.com/brigitabi/toDoGoals",
-        live:"https://to-do-goals-sjn7-frontend-brigitabi.vercel.app",
-    },
+export const data = [
+  {
+    id: 1,
+    name: 'Trello Clone with Chat GPT',
+    image: Planning,
+    github: 'https://github.com/brigitabi/NetflixClone',
+    live: 'https://trello-clone-piax.vercel.app',
+  },
+  {
+    id: 2,
+    name: 'Netflix Clone',
+    image: Netflix,
+    github: 'https://github.com/brigitabi/NetflixClone',
+    live: 'https://netflix-clone-bribi.web.app/',
+  },
+  {
+    id: 3,
+    name: 'Wisdom Wonder',
+    image: Wisdomwonder,
+    github: 'https://github.com/brigitabi/wisdom-wonder',
+    live: 'https://wisdom-wonder.vercel.app',
+  },
 
+  {
+    id: 4,
+    name: 'Hire & Admire',
+    image: Hireadmire,
+    github: 'https://github.com/brigitabi/H-A',
+    live: 'https://h-a.vercel.app',
+  },
 
-]
+  {
+    id: 5,
+    name: 'CoinNest',
+    image: Crypto,
+    github: 'Lawork',
+    live: 'https://cryptoexchange-2e20a.web.app/',
+  },
+  {
+    id: 6,
+    name: 'Global Beats',
+    image: GB2,
+    github: 'https://github.com/brigitabi/globalBeats',
+    live: 'https://global-beats.vercel.app/',
+  },
+  {
+    id: 7,
+    name: 'Expense Tracker',
+    image: Costs,
+    github: 'https://github.com/brigitabi/expenseTrack',
+    live: 'https://expense-track-ecru.vercel.app/',
+  },
+  {
+    id: 8,
+    name: 'We Eat',
+    image: Ramen,
+    github: 'https://github.com/brigitabi/weEat',
+    live: 'https://we-eat-gamma.vercel.app/',
+  },
+  {
+    id: 9,
+    name: 'Beautiful Image Slider',
+    image: Nature,
+    github: 'https://github.com/brigitabi/slider',
+    live: 'https://slider-flame.vercel.app',
+  },
+  {
+    id: 10,
+    name: 'Notes',
+    image: Goals,
+    github: 'https://github.com/brigitabi/toDoGoals',
+    live: 'https://to-do-goals-sjn7-frontend-brigitabi.vercel.app',
+  },
+];
