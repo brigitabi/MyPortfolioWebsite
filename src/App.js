e969f2c16697b4923f5adba0f9365ff1861fadfa
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-10-23 19:28:37 +0200

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Portfolio from "./components/Portfolio";
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import { db } from './firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
 // import Skills from "./components/Skills";
 
-
-
 function App() {
+  const [count, setCount] = React.useState();
+  const [fetched, setFetched] = React.useState(false);
+
+  const fetchCounter = async () => {
+    const docRef = doc(db, 'counter', 'count');
+
+    const response = await getDoc(docRef);
+
+    if (response.data()) {
+      setCount(response.data().count);
+    }
+
+    setFetched(true);
+  };
+
+  React.useEffect(() => {
+    if (!fetched) {
+      fetchCounter();
+    }
+  });
+
   return (
     <div>
-      <Navbar />
+      <Navbar countFromDb={count} />
       <Home />
       <About />
       {/* <Skills /> */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,104 @@
 import React from 'react';
-import {HiOutlineMail} from 'react-icons/hi';
-import {FaGithub, FaLinkedin} from 'react-icons/fa'; 
+import { HiOutlineMail } from 'react-icons/hi';
+import { FaGithub, FaLinkedin, FaEye } from 'react-icons/fa';
 import { Link } from 'react-scroll';
+import { db } from '../firebase';
+import { doc, setDoc } from 'firebase/firestore';
 
+const Navbar = ({ countFromDb }) => {
+  const [tracked, setTracked] = React.useState(false);
 
+  React.useEffect(() => {
+    const updateCounter = async () => {
+      const docRef = doc(db, 'counter', 'count');
+      const newValue = countFromDb + 1;
+      setTracked(true);
 
-const Navbar = () => {
+      await setDoc(docRef, { count: newValue });
+    };
+
+    if (countFromDb && !tracked) {
+      updateCounter();
+    }
+  }, [countFromDb, tracked]);
+
   return (
-<div className='fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a1527] text-white right-0'>
-    
-        <div className='flex justify-center items-center w-full'>
-            <ul className='flex space-x-6'>
-                <li>
-                    <Link to="about" smooth={true} duration={500} className='py-5 text-1.3xl'>About</Link>
-                </li>
-                <li>
-                <Link to="portfolio" smooth={true} duration={500} className='py-5 text-1.3xl'>Portolio</Link>
-                </li>
-                <li>
-                <Link to="contact" smooth={true} duration={500} className='py-5 text-1.3xl'>Contact</Link>
-                </li>
-            </ul>
-            </div>
+    <div className="fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a1527] text-white right-0">
+      <div className="flex justify-center items-center w-full">
+        <ul className="flex space-x-6">
+          <li>
+            <Link
+              to="about"
+              smooth={true}
+              duration={500}
+              className="py-5 text-1.3xl"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="portfolio"
+              smooth={true}
+              duration={500}
+              className="py-5 text-1.3xl"
+            >
+              Portolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              className="py-5 text-1.3xl"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-        {/*Social icons */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="flex items-center">
+        <FaEye className="mr-2" /> <span>{countFromDb}</span>
+      </div>
+
+      {/*Social icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1f1515]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1f1515]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/brigitabi' 
-              target='_blank'
-              rel='noopener noreferrer'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/brigitabi"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/brigita-bizjak-9472794b/'
-              target='_blank'
-              rel='noopener noreferrer'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/brigita-bizjak-9472794b/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#a14a95]'>
-            <Link to="contact"
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#a14a95]">
+            <Link
+              to="contact"
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Email <HiOutlineMail size={30} />
             </Link>
           </li>
         </ul>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
