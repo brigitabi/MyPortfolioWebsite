01e44a03090999a67a8d1b8608ccbdb5ce6f2857
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-10-23 23:41:39 +0200

    more styling :)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 '>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 lg:pt-24'>
         <form method='POST' action="https://getform.io/f/e2ce95c0-4077-489b-b259-19925fb31a06" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-700 text-gray-300'>Contact Me</p>
@@ -11,7 +11,7 @@ const Contact = () => {
             <input className='bg-[white] p-2' type="text" placeholder='Your Name' name='name' />
             <input className='my-4 p-2 bg-[white]' type="email" placeholder='Your Email' name='email' />
             <textarea className='bg-[white] p-3' name="message" rows="10" placeholder='Your Message'></textarea>
-            <button className='text-white border-2 hover:border-blue-950 hover:text-white px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+            <button className='text-white border-2 hover:bg-white hover:text-[darkblue] px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
         </form>
     </div>
   )
