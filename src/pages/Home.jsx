fe6a4340b789ecb02393cce464708967580bb918
Brigita Bizjak <brigita.bizjak05@gmail.com>
2024-03-07 13:53:19 +0100

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,20 @@ import { Link } from 'react-scroll';
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-white">
-      <div className="max-w-[1000px] mx-auto px-10 flex flex-col justify-center h-full">
-      
-        <h1 className="text-4xl sa:text-4xl font-bold text-[black] inline border-blue-800 w-1/2 py-8">
-        Hello there! I'm Brigita, a self-taught developer...
-        </h1>
-        <h2 className="text-xl sa:text-4xl font-thin text-black">
-         ...fueled by curiosity and a passion for learning. Join me as I navigate the ever-changing world of technology, always eager to tackle new challenges and create innovative solutions.
-        </h2>
-        <p className="text-[black] py-3 max-w-[800px]">
+      <div className="max-w-[1000px] mx-auto px-10 flex flex-col justify-center h-full ">
+        <div className='py-8'>
+          <h1 className="text-3xl  font-serif text-justify sa:text-4xl text-[black] inline border-blue-800 w-1/2 py-8">
+            Hello there! I'm Brigita, a self-taught developer...
+          </h1>
+          <h2 className="text-xl font-thin text-black py-8 ">
+            ...fueled by curiosity and a passion for learning. Join me as I
+            navigate the ever-changing world of technology, always eager to
+            tackle new challenges and create innovative solutions.
+          </h2>
+
           <div className="">
             <Link to="portfolio" smooth={true} duration={500}>
-              <button className="text-[black] border-2 px-6 font-bold py-3 my-2 flex items-center hover:bg-[#101221] hover:border-black hover:text-white">
+              <button className="text-[black] border-2 px-6 font-bold py-3 my-2 rounded-md flex items-center hover:bg-[#101221] hover:border-black hover:text-white">
                 View my projects
                 <span className="group-hover:rotate-90 duration-300">
                   <HiArrowNarrowRight className="ml-3 " />
@@ -25,15 +27,16 @@ const Home = () => {
               </button>
             </Link>
           </div>
-          <div className="flex justify-center mt-8">
-            <img
-              src={profile}
-              alt="profile"
-              style={{ width: '200px' }}
-              className={`rounded-full float-right px-5 animate-pulse`}
-            />
-          </div>
-        </p>
+        </div>
+
+        <div className="flex justify-center mt-8">
+          <img
+            src={profile}
+            alt="profile"
+            style={{ width: '200px' }}
+            className={`rounded-full float-right px-5 animate-pulse`}
+          />
+        </div>
       </div>
     </div>
   );
